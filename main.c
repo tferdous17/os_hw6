9c6c6609f4486fb0519b73b9d64f7154d04b7efb
tferdous17 <tferdous611@gmail.com>
2025-03-30 18:13:37 -0400

    complete hw

--- main.c
+++ main.c
@@ -1,7 +1,48 @@
 #include <stdio.h>
+#include <pthread.h>
 
-int main(void)
-{
-    printf("Hello, World!\n");
+int sharedData = 0;
+pthread_mutex_t mutex;
+
+void* writerThread(void* arg) {
+    pthread_mutex_lock(&mutex);
+    sharedData = 10;
+    printf("Writer: Wrote %d\n", sharedData);
+    pthread_mutex_unlock(&mutex);
+
+    return nullptr;
+}
+
+void* readerThread(void* arg) {
+    pthread_mutex_lock(&mutex);
+    printf("Reader: Read value %d\n", sharedData);
+    pthread_mutex_unlock(&mutex);
+    return nullptr;
+}
+
+int main(void) {
+    pthread_t thread1, thread2;
+
+    // initialize mutex
+    if (pthread_mutex_init(&mutex, nullptr) != 0) {
+        return 1;
+    }
+
+    // create threads
+    if (pthread_create(&thread1, nullptr, writerThread, nullptr) != 0) {
+        return 1;
+    }
+    if (pthread_create(&thread2, nullptr, readerThread, nullptr) != 0) { 
+        return 1;
+    }
+
+    // wait for threads to complete
+    pthread_join(thread1, nullptr);
+    pthread_join(thread2, nullptr);
+
+    // cleanup stuff
+    pthread_mutex_destroy(&mutex);
+    printf("Program completed\n");
+
     return 0;
 }
